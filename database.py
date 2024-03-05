ee8025ca75d65d171e03e2e831f742fd65bdff88
thebigbone <pacman@duck.com>
2024-03-05 18:40:35 +0530

    student and faculty list upload from admin panel

--- database.py
+++ database.py
@@ -4,12 +4,12 @@ from flask_mysqldb import MySQL
 mysql = MySQL()
 
 # MySQL configuration
+
+
 def configure_mysql(app):
-    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_HOST'] = '127.0.0.1'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'akshar'
+    app.config['MYSQL_PASSWORD'] = 'root'
     app.config['MYSQL_DB'] = 'wifiattendance'
 
     mysql.init_app(app)
-
-
